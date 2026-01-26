a55e6d36bcbec73eb80f8c0b55f51e4c900e0e4f
shawn-d-p <shawn.perera123d@gmail.com>
2026-01-26 21:38:40 +1100

    handle click
    when a square is pressed a x appears

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-export default function Square() {
+import { useState } from "react";
+
+//defines a single square to avoid duplicate code
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("x");
+  }
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+//defines the game board
+export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">x</button>
-        <button className="square">x</button>
-        <button className="square">x</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">x</button>
-        <button className="square">x</button>
-        <button className="square">x</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">x</button>
-        <button className="square">x</button>
-        <button className="square">x</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
