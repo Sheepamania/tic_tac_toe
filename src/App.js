aff4bfd19aec6344969442c954168074fdc75dc3
shawn-d-p <shawn.perera123d@gmail.com>
2026-01-27 13:46:20 +0530

    Add: handleClick function
    add handle click function to the parent component to enable inheretence

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
-//defines a single square to avoid duplicate code
-function Square({value}) {
-  return <button className="square">{value}</button>;
+// Defines a single square to avoid duplicate code
+// Pass in the value and functioin onSquareClick as component props
+function Square({value, onSqureClick}) {
+  return <button className="square" onClick={onSqureClick} >{value}</button>;
 }
 
 //defines the game board
@@ -10,22 +11,30 @@ export default function Board() {
 
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  function handleClick(){
+    const nextSquares = squares.slice();
+    nextSquares[0] = "x";
+
+    // Set the value of the "nextSquare"
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]}/>
-        <Square value={squares[2]}/>
+        <Square value={squares[0]} onSqureClick={handleClick} />
+        <Square value={squares[1]} onSqureClick={handleClick} />
+        <Square value={squares[2]} onSqureClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]}/>
-        <Square value={squares[4]}/>
-        <Square value={squares[5]}/>
+        <Square value={squares[3]} onSqureClick={handleClick} />
+        <Square value={squares[4]} onSqureClick={handleClick} />
+        <Square value={squares[5]} onSqureClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]}/>
-        <Square value={squares[7]}/>
-        <Square value={squares[8]}/>
+        <Square value={squares[6]} onSqureClick={handleClick} />
+        <Square value={squares[7]} onSqureClick={handleClick} />
+        <Square value={squares[8]} onSqureClick={handleClick} />
       </div>
     </>
   );
