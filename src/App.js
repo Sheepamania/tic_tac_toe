dcade76a11e34da5e5aa2e4e1be02c37290b7365
shawn-d-p <shawn.perera123d@gmail.com>
2026-02-15 16:06:53 +1100

    Finish
    Implemented jump back, which allows you to jump back to a previous move

--- src/App.js
+++ src/App.js
@@ -2,55 +2,106 @@ import { useState } from "react";
 
 // Defines a single square to avoid duplicate code
 // Pass in the value and functioin onSquareClick as component props
-function Square({value, onSqureClick}) {
-  return <button className="square" onClick={onSqureClick} >{value}</button>;
+function Square({ value, onSquareClick}) {
+  return <button className="square" onClick={onSquareClick} >{value}</button>;
 }
 
 //defines the game board
-export default function Board() {
-
-  //Each time a player moves, bool xIsNext will be flipped 
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({xIsNext, squares, onPlay}) {
 
   function handleClick(i){
     
-    if(squares[i]){
+    if(squares[i] || calculateWinner(squares)){
       return; 
     }
-
-    const nextSquares = squares.slice();
+    // Create a new copy of squares array after each move (Time travel)
+    const nextSquares = squares.slice();        
 
     if (xIsNext){
-      nextSquares[i] = "X";
+      nextSquares[i] = "X"; 
     } else {
       nextSquares[i] = "O";
     }
 
+    onPlay(nextSquares);
+  }
 
-    // Set the value of the "nextSquare"
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext); //flips the bool after each turn
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
   return (
     <>
+      <div className="status">{status}</div> 
       <div className="board-row">
-        <Square value={squares[0]} onSqureClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSqureClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSqureClick={() => handleClick(2)} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSqureClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSqureClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSqureClick={() => handleClick(5)} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSqureClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSqureClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSqureClick={() => handleClick(8)} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+
+// Export default is the top-level component that will be rendered 
+export default function Game() {
+
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove%2 === 0)
+  }
+
+  const moves = history.map((squares, move) => {
+    let description; 
+    if (move > 0){
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        {/*Pass as props to the board component*/}
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
 }
 
